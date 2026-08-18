17c4a390bdab4279947fa7e2027cf232fc9d06e0
Uma Iyer <152464173+uma-iyer-24@users.noreply.github.com>
2026-08-18 09:10:03 +0000

    demonstrated instance of class instead of functional component

--- fsd/week_5/week_5a/src/App.jsx
+++ fsd/week_5/week_5a/src/App.jsx
@@ -3,20 +3,14 @@ import React from 'react'
 // CLASS
 class MyClass extends React.Component {
   render() {
-    return <h2>Class Component</h2>
+    return <h2>This is an Instance of a Class Component</h2>
   }
-}
-
-// INSTANCE
-function MyFunction() {
-  return <h2>Functional Component</h2>
 }
 
 export default function App() {
   return (
     <div>
       <MyClass />
-      <MyFunction />
     </div>
   )
 }
